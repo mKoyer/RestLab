ac516cbc83020e68b0052a64141b80a118b5421b
Marcin S <marcin.koyer@gmail.com>
2018-06-26 03:51:00 +0200

    Rest poprawki

--- src/main/java/common/Student.java
+++ src/main/java/common/Student.java
@@ -1,8 +1,14 @@
 package common;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import endpoints.StudentsEndpoint;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import javax.ws.rs.core.Link;
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,10 +17,33 @@ import java.util.List;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Student
 {
+
+    @InjectLinks({
+            @InjectLink(resource = StudentsEndpoint.class, rel = "parent", method = "getStudents",style = InjectLink.Style.ABSOLUTE),
+            @InjectLink(resource = StudentsEndpoint.class, rel = "self", method = "getStudent", style = InjectLink.Style.ABSOLUTE)
+    })
+    @XmlElement(name = "links")
+    @XmlElementWrapper(name = "links")
+    @XmlJavaTypeAdapter(Link.JaxbAdapter.class)
+    List<Link> links;
+
+    public List<Link> getLinks()
+    {
+        return links;
+    }
+
+    public void setLinks(List<Link> links)
+    {
+        this.links = links;
+    }
+
     private int index;
     private String name;
     private String surname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date birthDate;
+    @JsonIgnore
+    @XmlTransient
     private List<Mark> marks;
 
     public Student()
